0790c7f8c4d6e05a6edf1c1d4546c7ba7cea8a5d
Josh Loecker <joshloecker@icloud.com>
2024-11-25 22:15:01 -0600

    remove .nvim from color scheme name

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,7 @@
 -- from: https://github.com/loctvl842/nvim/blob/master/lua/beastvim/plugins/colorscheme.lua
 return {
   {
-    "loctv842/monokai-pro.nvim",
+    "loctv842/monokai-pro",
     lazy = false,
     priority = 1000,
     config = function(_, opts)
