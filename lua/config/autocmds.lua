9ec4fe02892aef00dae94273de89febd1986264e
Josh Loecker <joshloecker@icloud.com>
2026-02-25 13:37:51 -0600

    move auto command to appropriate file

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,10 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Always open at start of file
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    vim.api.nvim_win_set_cursor(0, { 1, 0 })
+  end,
+})
