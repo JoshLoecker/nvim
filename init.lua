7da9dbdf991229917396cc7e23f5f51b74068c24
Josh Loecker <joshloecker@icloud.com>
2026-02-25 13:32:41 -0600

    always open file at beginning

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Always open at start of file
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    vim.api.nvim_win_set_cursor(0, { 1, 0 })
+  end,
+})
